706cf2bb5da36cfea6615656a0b4032205a9f9bf
Victor Ponamarev <vponamarev@guess.com>
2021-09-13 01:22:38 +0300

    Domain: AccountService added.

---
+++ src/GripItemTrade.Domain/Accounts/AccountService.cs
@@ -0,0 +1,15 @@
+﻿using GripItemTrade.Domain.Accounts.Interfaces;
+using GripItemTrade.Domain.Transactions;
+using System;
+using System.Collections.Generic;
+
+namespace GripItemTrade.Domain.Accounts
+{
+	public sealed class AccountService : IAccountService
+	{
+		public IReadOnlyList<TransactionalOperation> Transfer(Account sourceAccount, Account destinationAccount, ICollection<BalanceEntry> balanceEntries)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ src/GripItemTrade.Domain/Accounts/Interfaces/IAccountService.cs
@@ -0,0 +1,10 @@
+﻿using GripItemTrade.Domain.Transactions;
+using System.Collections.Generic;
+
+namespace GripItemTrade.Domain.Accounts.Interfaces
+{
+	public interface IAccountService
+	{
+		IReadOnlyList<TransactionalOperation> Transfer(Account sourceAccount, Account destinationAccount, ICollection<BalanceEntry> balanceEntries);
+	}
+}
